3463e73bb202dc6a1bf155964515e2790ebb45db
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-05-22 00:24:27 -0700

    implement onStart/Restart/Stop() and onDestroy()

--- app/src/main/java/com/irdaislakhuafa/myfirstandroidappinjava/MainActivity.java
+++ app/src/main/java/com/irdaislakhuafa/myfirstandroidappinjava/MainActivity.java
@@ -26,6 +26,30 @@ public class MainActivity extends AppCompatActivity {
         showLogs("onResume");
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        showLogs("onStart");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        showLogs("onRestart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        showLogs("onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        showLogs("onDestroy");
+    }
+
     private static void showLogs(String value) {
         Log.d("MyAppMessage", "Running method " + value.trim() + "(1412190011)");
     }
